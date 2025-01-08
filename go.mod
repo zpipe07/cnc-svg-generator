e0f31af29319a37e1f1c9a61ff63a0035a989c49
Zack Piper <zpipe07@gmail.com>
2025-01-08 11:42:57 -0700

    return text as svg

--- go.mod
+++ go.mod
@@ -9,7 +9,6 @@ require (
 
 require (
 	github.com/ByteArena/poly2tri-go v0.0.0-20170716161910-d102ad91854f // indirect
-	github.com/Kagami/go-avif v0.1.0 // indirect
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/benoitkugler/textlayout v0.3.0 // indirect
 	github.com/benoitkugler/textprocessing v0.0.3 // indirect
@@ -20,16 +19,15 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-fonts/latin-modern v0.3.3 // indirect
-	github.com/go-latex/latex v0.0.0-20240709081214-31cef3c7570e // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/go-text/typesetting v0.2.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
-	github.com/kolesa-team/go-webp v1.0.4 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -40,14 +38,12 @@ require (
 	github.com/tdewolff/parse/v2 v2.7.19 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
-	github.com/wcharczuk/go-chart/v2 v2.1.2 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/image v0.22.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
-	gonum.org/v1/plot v0.15.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	star-tex.org/x/tex v0.5.0 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,39 +10,59 @@ import (
 	"github.com/tdewolff/canvas/renderers/svg"
 )
 
-func main() {
-	r := gin.Default()
+//go:embed fonts/Bungee-Regular.ttf
+var bungeeFont []byte
 
-	r.GET("/svg", func(c *gin.Context) {
-		svgContent := generateSVG()
-		c.Data(http.StatusOK, "image/svg+xml", []byte(svgContent))
-	})
+func generateSVG(text string) string {
 
-	r.Run(":8080") // Start the server on port 8080
-}
-
-func generateSVG() string {
 	// Create a new canvas with a defined width and height (e.g., 100x100 mm)
-	c := canvas.New(100, 100)
+	c := canvas.New(200, 200)
 	ctx := canvas.NewContext(c)
 
-	// Draw some shapes on the canvas
-	ctx.SetFillColor(canvas.Lightblue)
-	// ctx.DrawRect(10, 10, 80, 80) // Draw a rectangle
-	ctx.DrawPath(10, 10, canvas.Rectangle(80, 80)) // Draw a rectangle
+	// Draw text if provided
+	if text != "" {
+		ctx.SetFillColor(canvas.Black)
+		fontFamily := canvas.NewFontFamily("Bungee")
+		err := fontFamily.LoadFont(bungeeFont, 0, canvas.FontRegular)
+		if err != nil {
+			panic("Failed to load font: " + err.Error())
+		}
+		face := fontFamily.Face(44, canvas.FontRegular, canvas.FontNormal)
 
-	ctx.SetStrokeColor(canvas.Black)
-	ctx.SetStrokeWidth(2)
-	ctx.DrawPath(10, 10, canvas.Line(80, 80)) // Draw a diagonal line
+		// Convert text to a path
+		textPath, _, err := face.ToPath(text)
+		if err != nil {
+			panic("Failed to convert text to path: " + err.Error())
+		}
+		textBounds := textPath.Bounds()
+		textX := 100 - textBounds.W()/2
+		textY := 100 - textBounds.H()/2
 
-	ctx.SetStrokeColor(canvas.Red)
-	ctx.DrawPath(50, 50, canvas.Circle(30)) // Draw a circle
+		// Move the text path to the desired position
+		textPath = textPath.Translate(textX, textY)
+
+		// Draw the text path
+		ctx.SetFillColor(canvas.Black)
+		ctx.DrawPath(0, 0, textPath)
+	}
 
 	// Export the canvas to an SVG
 	var buf bytes.Buffer
-	svgCanvas := svg.New(&buf, 100, 100, nil)
+	svgCanvas := svg.New(&buf, 200, 200, nil)
 	c.RenderTo(svgCanvas)
 	svgCanvas.Close()
 
 	return buf.String()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/svg", func(c *gin.Context) {
+		text := c.Query("text")
+		svgContent := generateSVG(text)
+		c.Data(http.StatusOK, "image/svg+xml", []byte(svgContent))
+	})
+
+	r.Run(":8080") // Start the server on port 8080
 }
